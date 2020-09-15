5cf10794aa582484c72c1c912e78e901823a8f7a
Luis García <aig.luis@gmail.com>
2020-09-15 10:17:16 -0500

    Updates

---
+++ precache-manifest.fd582d669b0f26e5ea2ecf3eab76d268.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8de4a36fca7058b57d93fd6fd95542",
+    "url": "/fundacionsonrisa/index.html"
+  },
+  {
+    "revision": "7727c027e3aa6ea521de",
+    "url": "/fundacionsonrisa/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5c19deb30e703b7db9d0",
+    "url": "/fundacionsonrisa/static/css/main.4bfbe09e.chunk.css"
+  },
+  {
+    "revision": "7727c027e3aa6ea521de",
+    "url": "/fundacionsonrisa/static/js/2.2f51f49e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fundacionsonrisa/static/js/2.2f51f49e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c19deb30e703b7db9d0",
+    "url": "/fundacionsonrisa/static/js/main.a41cd738.chunk.js"
+  },
+  {
+    "revision": "c8bfc7509fc5690d8c68",
+    "url": "/fundacionsonrisa/static/js/runtime-main.897e446e.js"
+  },
+  {
+    "revision": "24415b9582e8681c624e27ee38779827",
+    "url": "/fundacionsonrisa/static/media/foto.24415b95.png"
+  },
+  {
+    "revision": "5003985f8434e5ff7cbd33ae4b020d53",
+    "url": "/fundacionsonrisa/static/media/logo.5003985f.png"
+  }
+]);
